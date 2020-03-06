9b52579caa2f8e33e63e87b8cfac23fe65ab3b5f
bourbon-vanilla <bourbon-vanilla@outlook.com>
2020-03-06 18:33:15 +0100

    Add swashbuckle package; Add code to activate swagger;

--- SwashbuckleExample/Startup.cs
+++ SwashbuckleExample/Startup.cs
@@ -15,10 +15,18 @@ namespace SwashbuckleExample
 {
     public class Startup
     {
+        private const string OPEN_API_SPECIFICATION_NAME = "open-api-spec";
+        private const string API_NAME = "The API";
+
+        private string _openApiSpecAddress = $"/swagger/{OPEN_API_SPECIFICATION_NAME}/swagger.json";
+        private string _uiAddress = $"/swagger/index.html";
+
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
+
 
         public IConfiguration Configuration { get; }
 
@@ -26,6 +34,16 @@ namespace SwashbuckleExample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(setupAction =>
+            {
+                setupAction.SwaggerDoc(OPEN_API_SPECIFICATION_NAME,
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = API_NAME,
+                        Version = "1"
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +63,15 @@ namespace SwashbuckleExample
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(setupAction =>
+            {
+                setupAction.SwaggerEndpoint(
+                    _openApiSpecAddress,
+                    API_NAME);
             });
         }
     }
